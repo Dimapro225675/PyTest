975174bae629a99bc892b912e331ab14845d0417
Ilya <ilamajor21099@gmail.com>
2025-12-04 13:54:21 +0300

    first commit

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -5,7 +5,15 @@ def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 3
     assert arrs.get([], 0, "test") == "test"
 
+def test_get_out_of_range_index(self):
+    assert arrs.get([1, 2, 3], 5) is None
+    assert arrs.get([1, 2, 3], 10) is None
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+
+def test_slice_start_greater_than_end(self):
+    assert arrs.my_slice([1, 2, 3, 4, 5], 3, 2) == []
+    assert arrs.my_slice([1, 2, 3, 4, 5], 4, 1) == []
+    assert arrs.my_slice([1, 2, 3, 4, 5], 4, 1) == []
